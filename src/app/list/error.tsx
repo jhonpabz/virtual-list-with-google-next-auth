29dab88bf7b6906b6a5d769aaef979b543730a32
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-05-26 21:24:56 +0800

    SSR List page. Added error and loading UI.

---
+++ src/app/list/error.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Error() {
+  return <h1>There is something wrong.</h1>;
+}
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,10 +1,22 @@
-import styles from '../page.module.css';
-import { VirtualList } from '@/components';
+async function getData() {
+  const res = await fetch('https://dummyjson.com/products?limit=100');
 
-export default function List() {
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  console.log(res);
+
+  return res.json();
+}
+
+export default async function List() {
+  const products = await getData();
+
   return (
-    <main className={styles.main}>
-      <VirtualList />
+    <main>
+      {products.products.map((product: any) => {
+        return <p key={product.id}>{product.title}</p>;
+      })}
     </main>
   );
 }
