d3db7fd8e891f7a75775f6f3be3784f6cb4e76d4
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-07-15 14:45:08 +0800

    Implement GitHubProvider and CredentialsProvider.

--- src/library/routes.ts
+++ src/library/routes.ts
@@ -1,22 +1,27 @@
 export const routes = [
   {
-    name: 'Home',
-    path: '/',
+    name: "Home",
+    path: "/",
   },
   {
-    name: 'About',
-    path: '/about',
+    name: "About",
+    path: "/about",
   },
   {
-    name: 'List',
-    path: '/list',
+    name: "List",
+    path: "/list",
   },
   {
-    name: 'Profile (Protected Route)',
-    path: '/profile',
+    name: "Profile (Protected Route)",
+    path: "/profile",
   },
   {
-    name: 'Sign In',
-    path: '/signin',
+    name: "Sign In",
+    path: "/api/auth/signin",
+  },
+
+  {
+    name: "Logout",
+    path: "/api/auth/signout",
   },
 ];
---
+++ src/middleware.ts
@@ -0,0 +1,2 @@
+// Add this to make all pages in app protected
+export { default } from "next-auth/middleware";
